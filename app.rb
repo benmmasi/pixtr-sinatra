9c01a67afce5ce247eecae434a19483cc6a4a63c
Ben Masi <ben.m.masi@gmail.com>
2014-09-24 14:15:59 -0400

    Using pg and using active_record

--- app.rb
+++ app.rb
@@ -2,22 +2,29 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pg"
 
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "photo_gallery"
+  )
 database = PG.connect({ dbname: "photo_gallery"})
 
 get "/" do
-  @gallery_names = database.exec_params({ name: "photo_gallery" }) 
-  @gallery_names.each do |name|
-    @gallery_names << ["name"]
-  end
-  
+  gallery_result = database.exec_params("SELECT name FROM galleries") 
+  @gallery_names = gallery_result.map do |gallery|
+    gallery["name"]
+  end 
+
   erb :home
 end
 
-get "/gallery/:name" do
-  @name = params[:name]
-  @images = GALLERIES[@name]
-  erb :gallery
+get "/galleries/:id" do
+  id = params[:id]
+  name_result = database.exec_params("SELECT name FROM galleries WHERE id = $1", [id])
+  @name = name_result.first["name"]
+  @images = []
+  erb :galleries
 end
+
 
 
 
